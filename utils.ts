d6d9a928b552ab592356cfda5da0dfabcabf95b4
Charlie L <charliesbot@gmail.com>
2020-12-02 20:02:48 -0800

    solve day 2

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 const getInputLines = async (fileName: string): Promise<Array<string>> => {
   const text = await Deno.readTextFile(fileName);
-  return text.split("\n");
+  return text.split("\n").filter((t: string | undefined) => t);
 };
 
 export { getInputLines };
