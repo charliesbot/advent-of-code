3dfe8af9d3fe53e0ecf63825107289a599392b47
Charlie L <charliesbot@gmail.com>
2020-12-06 14:53:34 -0800

    day 4

--- utils.ts
+++ utils.ts
@@ -3,4 +3,10 @@ const getInputLines = async (fileName: string): Promise<Array<string>> => {
   return text.split("\n").filter((t: string | undefined) => t);
 };
 
-export { getInputLines };
+const getParagraphs = async (fileName: string) => {
+  const lines = await Deno.readTextFile(fileName);
+  const paragraphs = lines.split("\n\n").map((l) => l.split("\n").join(" "));
+  return paragraphs;
+};
+
+export { getInputLines, getParagraphs };
